b23df7360cfbd00b897cc357c989d6282405dbcf
Paul Ridgway <paul@ridgway.io>
2019-02-10 16:17:02 +0000

    Email templating

--- functions/notify/lambda.rb
+++ functions/notify/lambda.rb
@@ -2,45 +2,75 @@ require 'json'
 require 'aws-sdk'
 require 'logger'
 
-logger = Logger.new(STDOUT)
-logger.level = Logger::INFO
+class Notify
 
-def send_email(from:, to:, subject:, html:, text:)
-  ses = Aws::SES::Client.new(region: 'eu-west-1')
+  L = begin
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::INFO
+    logger
+  end
 
-  to = to.is_a?(Array) ? to : [to]
-  begin
-    options = {
-      destination: {
-        to_addresses: to
-      },
-      message: {
-        body: {
-          html: {
-            charset: 'UTF-8',
-            data: html
+  def run
+    L.info("run")
+
+    subject = 'AWS Billing Alert'
+
+    html = process_template(template_file: 'template.html', substitutions: {subject: subject, balance: 0.99, checked: 'just now'})
+    text = ''
+    self.send_email(from: 'billing-alerts@ridgway.io', to: 'paul@ridgway.io', subject: subject,
+                    text: text, html: html)
+  end
+
+  def process_template(template_file:, substitutions:)
+    template = File.read(template_file)
+    matches = template.scan(/\$\$([A-Z]{1,20})\$\$/)
+    matches = Hash[matches.flatten.uniq.map {|m| [m.to_s.downcase.to_sym, m]}]
+    missing = matches.keys - substitutions.keys
+    if missing.size > 0
+      L.warn("Missing template keys in substitutions: #{missing.join(', ')}")
+    end
+    matches.each {|sym, str| template.gsub!("$$#{str}$$", substitutions[sym].to_s)}
+    template
+  end
+
+  def send_email(from:, to:, subject:, html:, text:)
+    ses = Aws::SES::Client.new(region: 'eu-west-1')
+
+    to = to.is_a?(Array) ? to : [to]
+    begin
+      options = {
+        destination: {
+          to_addresses: to
+        },
+        message: {
+          body: {
+            html: {
+              charset: 'UTF-8',
+              data: html
+            },
+            text: {
+              charset: 'UTF-8',
+              data: text
+            }
           },
-          text: {
+          subject: {
             charset: 'UTF-8',
-            data: text
+            data: subject
           }
         },
-        subject: {
-          charset: 'UTF-8',
-          data: subject
-        }
-      },
-      source: from
-    }
-    resp = ses.send_email(options)
-    logger.info('Email sent!')
-  rescue Aws::SES::Errors::ServiceError => error
-    logger.error("Email not sent. Error message: #{error}")
-    raise error
+        source: from
+      }
+      resp = ses.send_email(options)
+      L.info('Email sent!')
+    rescue Aws::SES::Errors::ServiceError => error
+      L.error("Email not sent. Error message: #{error}")
+      raise error
+    end
   end
+
 end
 
-def handler(event:, context:)
+def handler(event: nil, context: nil)
   # The event payload will look something like this
   # {
   #   "version": "0",
@@ -54,7 +84,12 @@ def handler(event:, context:)
   #   "detail": {}
   # }
 
-  send_email(sender: 'billing-alerts@ridgway.io', recipient: 'paul@ridgway.io', subject: 'AWS Billing Alert', text: 'Some body', html: 'Some html')
+  Notify.new.run
 
   {statusCode: 200, body: JSON.generate('Hello from Lambda!')}
+end
+
+unless ENV['AWS_LAMBDA_FUNCTION_NAME']
+  puts "Not in lambda, running locally!"
+  handler(event: {}, context: {})
 end
