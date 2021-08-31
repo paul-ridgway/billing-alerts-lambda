0da48fee5a72bb2dd8a5854a8dec440f4a2c9113
Paul Ridgway <paul@ridgway.io>
2021-08-31 14:48:38 +0100

    fixes

--- scripts/s3-deploy.sh
+++ scripts/s3-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit 1
 ./zip.sh
-aws s3 cp ../functions/notify/billing-alerts_notify.zip s3://5052/billing-alerts_notify.zip
+aws s3 cp --acl public-read ../functions/notify/billing-alerts_notify.zip s3://5052/billing-alerts_notify.zip
 rm -r ../functions/notify/billing-alerts_notify.zip
